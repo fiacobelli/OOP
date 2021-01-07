bb7b9e4e9295d631e72cccc7f5075acb7493470b
fiacobelli <francisco@iacobelli.cl>
2021-01-07 14:13:23 -0600

    first task

---
+++ inheritance/task1/src/Square.java
@@ -0,0 +1,29 @@
+public class Square extends Shape{
+    double side;
+    public Square(double side,String name){
+        super(name);
+        this.side=side;
+    }
+
+    @Override
+    public double getArea(){return this.side*this.side;}
+
+    @Override
+    public double getPerimeter() {
+        return this.side * 4;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj!=null && obj.getClass() == this.getClass()){
+            Square s = (Square)obj;
+            return this.name.equals(s.name) && this.side==s.side;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return this.name+":"+this.side;
+    }
+}
